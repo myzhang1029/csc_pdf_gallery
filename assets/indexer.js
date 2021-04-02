836e5733244b5e90af247967d75e032a6466cec2
Zhang Maiyun <myzhang1029@hotmail.com>
2021-04-02 15:33:35 +0800

    Add background and resize btn
    Signed-off-by: Zhang Maiyun <myzhang1029@hotmail.com>

--- assets/indexer.js
+++ assets/indexer.js
@@ -4,7 +4,7 @@ var items_per_slide_height;
 var items_per_slide;
 update_items_per_slide();
 
-function Zoom(size) {
+function zoom(size) {
     if (size > 0 && items_per_slide_width != 1) {
         items_per_slide_width -= 1; /* Avoid zooming in to zero items */
     }
@@ -18,10 +18,10 @@ function Zoom(size) {
 document.addEventListener('keydown', function (event) {
     if (event.code == "Plus" ||
         event.code == "Equal" && event.shiftKey == true) {
-        Zoom(1);
+        zoom(1);
     }
     else if (event.code == "Minus") {
-        Zoom(-1);
+        zoom(-1);
     }
 });
 
@@ -176,84 +176,3 @@ function displaySlides(n) {
         }
     }
 }
-
-
-// These are from https://developer.mozilla.org/en-US/docs/Web/API/Pointer_events/Pinch_zoom_gestures
-// For pinching events
-var evCache = new Array();
-var prevDiff = -1;
-function pointDown(event) {
-    evCache.push(event);
-}
-
-function pointMove(ev) {
-    // This function implements a 2-pointer horizontal pinch/zoom gesture.
-    //
-    // If the distance between the two pointers has increased (zoom in),
-    // the target element's background is changed to "pink" and if the
-    // distance is decreasing (zoom out), the color is changed to "lightblue".
-    //
-    // This function sets the target element's border to "dashed" to visually
-    // indicate the pointer's target received a move event.
-    console.log("pointerMove", ev);
-
-    // Find this event in the cache and update its record with this event
-    for (var i = 0; i < evCache.length; i++) {
-        if (ev.pointerId == evCache[i].pointerId) {
-            evCache[i] = ev;
-            break;
-        }
-    }
-
-    // If two pointers are down, check for pinch gestures
-    if (evCache.length == 2) {
-        // Calculate the distance between the two pointers
-        var curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
-
-        if (prevDiff > 0) {
-            if (curDiff > prevDiff) {
-                // The distance between the two pointers has increased
-                console.log("Pinch moving OUT -> Zoom in", ev, curDiff -  prevDiff);
-            }
-            if (curDiff < prevDiff) {
-                // The distance between the two pointers has decreased
-                console.log("Pinch moving IN -> Zoom out", ev, curDiff - prevDiff);
-            }
-            Zoom((curDiff - prevDiff));
-        }
-
-        // Cache the distance for the next move event
-        prevDiff = curDiff;
-    }
-    window.innerWidth = screen.width;
-    document.body.style.zoom = "100%";
-}
-
-function remove_event(ev) {
-    // Remove this event from the target's cache
-    for (var i = 0; i < evCache.length; i++) {
-        if (evCache[i].pointerId == ev.pointerId) {
-            evCache.splice(i, 1);
-            break;
-        }
-    }
-}
-
-function pointUp(ev) {
-    console.log(ev.type, ev);
-    // Remove this pointer from the cache and reset the target's
-    // background and border
-    remove_event(ev);
-
-    // If the number of pointers down is less than two then reset diff tracker
-    if (evCache.length < 2) {
-        prevDiff = -1;
-    }
-}
-
-document.onpointerdown = pointDown;
-document.onpointermove = pointMove;
-document.onpointerup = pointUp;
-document.onpointercancel = pointUp;
-document.onpointerout = pointUp;
-document.onpointerleave = pointUp;
